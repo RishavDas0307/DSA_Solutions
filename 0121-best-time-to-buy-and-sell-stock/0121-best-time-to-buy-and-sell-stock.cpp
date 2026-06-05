e58baeec4835022b7304d5e96c398d0b1da4589f
Rishav Kumar Das <rishav.k.das@gmail.com>
2026-06-05 11:29:12 +0530

    Time: 0 ms (100%), Space: 97.4 MB (59.84%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=INT_MAX;
+        int max=0;
+        for (int price:prices){
+            if (price<min){
+                min=price;
+            }
+            else if (price-min>max){
+                max=price-min;
+            }
+        }
+        return max;
+    }
+};
