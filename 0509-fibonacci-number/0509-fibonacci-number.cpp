9cea0dd4206d9869ebdde9cb851400395d05be53
Rishav Kumar Das <rishav.k.das@gmail.com>
2026-06-18 10:05:36 +0530

    Time: 2 ms (78.01%), Space: 7.6 MB (98.61%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n==0) {return 0;}
+        int prev2=0;
+        int prev1=1;
+        int curr;
+        for (int i=2; i<n+1; i++){
+            curr=prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
